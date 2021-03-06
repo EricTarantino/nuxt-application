5e1ade3d8699444789a739c59c390cfd9d172be2
EricTarantino <eric.vanlessen@live.de>
2021-03-06 17:22:35 +0100

    adfs-security

---
+++ nuxt.config.js
@@ -0,0 +1,102 @@
+
+module.exports = {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxt/typescript-build',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
+  router: {
+    middleware: ['auth']
+  },
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  },
+  auth: {
+    redirect: {
+      login: '/',
+      callback: '/auth'
+    },
+    strategies: {
+      aad: {
+        scheme: 'oauth2',
+        endpoints: {
+          authorization: 'https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize',
+          token: 'https://login.microsoftonline.com/organizations/oauth2/v2.0/token',
+          userInfo: '',
+          logout: '/'
+        },
+        token: {
+          property: 'access_token',
+          type: 'Bearer',
+          maxAge: 1800
+        },
+        refreshToken: {
+          property: 'refresh_token',
+          maxAge: 60 * 60 * 24 * 30
+        },
+        responseType: 'code',
+        grantType: 'authorization_code',
+        accessType: 'offline',
+        // ******** change this for your Application (Client) ID ********
+        clientId: '2e4e2fb2-67de-4b52-b706-eeae227b8256',
+        codeChallengeMethod: 'S256',
+        scope: ['openid', 'profile'],
+        autoLogout: true
+      }
+    }
+  }
+}
+
